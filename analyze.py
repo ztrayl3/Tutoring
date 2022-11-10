bd916cbfdc22f33a835fee762e6474358e6ff075
zacha <ztrayl3@lsu.edu>
2022-11-10 17:10:25 +0100

    Post session changes for Armen

--- analyze.py
+++ analyze.py
@@ -2,126 +2,144 @@ import matplotlib
 import pandas
 import mne
 
+
 # read raw EEG file
-eeg_path = "Data/emotion/emotion-offline-[2022.11.08-12.22.22].edf"
-raw = mne.io.read_raw_edf(eeg_path, preload=True)
+files = ["Data/emotion/raw_exp1.edf", "Data/emotion/raw_exp2.edf", "Data/emotion/raw_exp3.edf",
+         "Data/emotion/raw_exp4.edf", "Data/emotion/raw_exp5.edf", "Data/emotion/raw_exp6.edf"]
+raws = []
 
-# read valence and arousal responses
-labels = pandas.read_csv("Data/emotion/0_offline.csv")
+for eeg_path in files:
+    raw = mne.io.read_raw_edf(eeg_path, preload=True)
+    montage = mne.channels.make_standard_montage('standard_1020')  # load the standard 10-20
+    raw.set_montage(montage, on_missing="ignore")  # apply the channel montage
+    raws.append(raw)
 
-# provide accurate channel names
-channels = ["F7", "Fp1", "Fp2", "F8", "F3", "Fz", "F4", "C3", "Cz", "P8", "P7", "Pz", "P4", "T3", "P3", "O1", "O2",
-            "C4", "T4"]
+"""
+for eeg_path in files:
+    raw = mne.io.read_raw_edf(eeg_path, preload=True)
 
-# rename channels (only eeg channels [:19])
-mne.rename_channels(raw.info, dict(zip(raw.ch_names[:19], channels)))
+    # read valence and arousal responses
+    labels = pandas.read_csv("Data/emotion/0_offline.csv")
 
-# set montage and channel types
-montage = mne.channels.make_standard_montage('standard_1020')  # load the standard 10-20
+    # provide accurate channel names
+    channels = ["F7", "Fp1", "Fp2", "F8", "F3", "Fz", "F4", "C3", "Cz", "P8", "P7", "Pz", "P4", "T3", "P3", "O1", "O2",
+                "C4", "T4"]
 
-new_types = []
-for i in raw.ch_names:  # for each channel that we have in raw...
-    if i in channels:  # if it is a 10-20 channel name...
-        new_types.append("eeg")  # label it as EEG
-    else:  # if it isn't 10-20...g
-        new_types.append("misc")  # label it as misc
-raw.set_channel_types(dict(zip(raw.ch_names, new_types)))  # apply the new channel types
-raw.set_montage(montage, on_missing="ignore")  # apply the channel montage
+    # rename channels (only eeg channels [:19])
+    mne.rename_channels(raw.info, dict(zip(raw.ch_names[:19], channels)))
 
-# only pick the eeg channels
-raw.pick(["eeg"])
+    # set montage and channel types
+    montage = mne.channels.make_standard_montage('standard_1020')  # load the standard 10-20
 
-# map names of annotations to their ID
-mapping = {'OVTK_GDF_End_Of_Trial': 800,
-           'OVTK_StimulationId_BaselineStart': 32775,
-           'OVTK_StimulationId_BaselineStop': 32776,
-           'OVTK_StimulationId_ExperimentStart': 32769,
-           'OVTK_StimulationId_ExperimentStop': 32770,
-           'OVTK_StimulationId_Label_01': 33025,
-           'OVTK_StimulationId_Label_02': 33026,
-           'OVTK_StimulationId_Label_03': 33027,
-           'OVTK_StimulationId_Label_04': 33028,
-           'OVTK_StimulationId_Label_05': 33029}
+    new_types = []
+    for i in raw.ch_names:  # for each channel that we have in raw...
+        if i in channels:  # if it is a 10-20 channel name...
+            new_types.append("eeg")  # label it as EEG
+        else:  # if it isn't 10-20...g
+            new_types.append("misc")  # label it as misc
+    raw.set_channel_types(dict(zip(raw.ch_names, new_types)))  # apply the new channel types
+    raw.set_montage(montage, on_missing="ignore")  # apply the channel montage
 
-# create events from annotations
-events, event_dict = mne.events_from_annotations(raw)#, mapping)
+    # only pick the eeg channels
+    raw.pick(["eeg"])
 
-# extract the annotations as a dataframe to iterate over
-anot = raw.annotations.to_data_frame()
+    # map names of annotations to their ID
+    mapping = {'OVTK_GDF_End_Of_Trial': 800,
+               'OVTK_StimulationId_BaselineStart': 32775,
+               'OVTK_StimulationId_BaselineStop': 32776,
+               'OVTK_StimulationId_ExperimentStart': 32769,
+               'OVTK_StimulationId_ExperimentStop': 32770,
+               'OVTK_StimulationId_Label_01': 33025,
+               'OVTK_StimulationId_Label_02': 33026,
+               'OVTK_StimulationId_Label_03': 33027,
+               'OVTK_StimulationId_Label_04': 33028,
+               'OVTK_StimulationId_Label_05': 33029}
 
-# create mappings from integer response to high/low valence/arousal
-valence_mapping = {1: "OVTK_StimulationId_Label_00", 2: "OVTK_StimulationId_Label_00", 3: "OVTK_StimulationId_Label_00",
-                   4: "OVTK_StimulationId_Label_00", 5: "OVTK_StimulationId_Label_01", 6: "OVTK_StimulationId_Label_01",
-                   7: "OVTK_StimulationId_Label_01", 8: "OVTK_StimulationId_Label_01", 9: "OVTK_StimulationId_Label_01"}
-arousal_mapping = {1: "OVTK_StimulationId_Label_10", 2: "OVTK_StimulationId_Label_10", 3: "OVTK_StimulationId_Label_10",
-                   4: "OVTK_StimulationId_Label_10", 5: "OVTK_StimulationId_Label_11", 6: "OVTK_StimulationId_Label_11",
-                   7: "OVTK_StimulationId_Label_11", 8: "OVTK_StimulationId_Label_11", 9: "OVTK_StimulationId_Label_11"}
+    # create events from annotations
+    events, event_dict = mne.events_from_annotations(raw)#, mapping)
 
-# these are the videos we are interested in
-video_labels = [33025, 33026, 33027, 33028, 33029]
+    # extract the annotations as a dataframe to iterate over
+    anot = raw.annotations.to_data_frame()
 
-# create empty lists for the 3 columns
-onsets = []
-durations = []
-descriptions = []
-# identify start time
-start_time = anot["onset"][0]
+    # create mappings from integer response to high/low valence/arousal
+    valence_mapping = {1: "OVTK_StimulationId_Label_00", 2: "OVTK_StimulationId_Label_00", 3: "OVTK_StimulationId_Label_00",
+                       4: "OVTK_StimulationId_Label_00", 5: "OVTK_StimulationId_Label_01", 6: "OVTK_StimulationId_Label_01",
+                       7: "OVTK_StimulationId_Label_01", 8: "OVTK_StimulationId_Label_01", 9: "OVTK_StimulationId_Label_01"}
+    arousal_mapping = {1: "OVTK_StimulationId_Label_10", 2: "OVTK_StimulationId_Label_10", 3: "OVTK_StimulationId_Label_10",
+                       4: "OVTK_StimulationId_Label_10", 5: "OVTK_StimulationId_Label_11", 6: "OVTK_StimulationId_Label_11",
+                       7: "OVTK_StimulationId_Label_11", 8: "OVTK_StimulationId_Label_11", 9: "OVTK_StimulationId_Label_11"}
 
-# iterate through original annotation dataframe
-for index, row in anot.iterrows():
+    # these are the videos we are interested in
+    video_labels = [33025, 33026, 33027, 33028, 33029]
 
-    # identify annotation id
-    anot_id = mapping[row["description"]]
+    # create empty lists for the 3 columns
+    onsets = []
+    durations = []
+    descriptions = []
+    # identify start time
+    start_time = anot["onset"][0]
 
-    # find the videos we are interested in 
-    if anot_id in video_labels:
+    # iterate through original annotation dataframe
+    for index, row in anot.iterrows():
 
-        # calculate the duration of this video
-        duration = (anot["onset"][index + 1] - anot["onset"][index]).total_seconds()
+        # identify annotation id
+        anot_id = mapping[row["description"]]
 
-        # extract the reported valence
-        val_reported = labels[labels['Unnamed: 0'] == anot_id]["Valence"]
-        # map the reported calence to low or high valence
-        val_mapped = valence_mapping[int(val_reported)]
+        # find the videos we are interested in
+        if anot_id in video_labels:
 
-        # add appropriate values for the 3 columns
-        onsets += [(row["onset"] - start_time).total_seconds() + 0.01]
-        durations += [duration]
-        descriptions += [val_mapped]
+            # calculate the duration of this video
+            duration = (anot["onset"][index + 1] - anot["onset"][index]).total_seconds()
 
-        # repeat the process for arousal
-        ar_reported = labels[labels['Unnamed: 0'] == anot_id]["Arousal"]
-        ar_mapped = arousal_mapping[int(ar_reported)]
+            # extract the reported valence
+            val_reported = labels[labels['Unnamed: 0'] == anot_id]["Valence"]
+            # map the reported calence to low or high valence
+            val_mapped = valence_mapping[int(val_reported)]
 
-        onsets += [(row["onset"] - start_time).total_seconds() + 0.02]
-        durations += [duration]
-        descriptions += [ar_mapped]
+            # add appropriate values for the 3 columns
+            onsets += [(row["onset"] - start_time).total_seconds() + 0.01]
+            durations += [duration]
+            descriptions += [val_mapped]
 
-    # if the annotation is not a video then just copy it down
-    else:
-        onsets += [(row["onset"] - start_time).total_seconds()]
-        durations += [row["duration"]]
-        descriptions += [row["description"]]
+            # repeat the process for arousal
+            ar_reported = labels[labels['Unnamed: 0'] == anot_id]["Arousal"]
+            ar_mapped = arousal_mapping[int(ar_reported)]
 
-# create a dataframe for the new annotations
-emotion_annotations = pandas.DataFrame(data=[onsets, durations, descriptions]).transpose()
-# rename the columns
-emotion_annotations = emotion_annotations.rename(columns={0: "onset", 1: "duration", 2: "description"})
+            onsets += [(row["onset"] - start_time).total_seconds() + 0.02]
+            durations += [duration]
+            descriptions += [ar_mapped]
 
-# set the new annotations for the raw file
-raw_annotations = mne.Annotations(onset=list(emotion_annotations["onset"]),  # onset, in seconds
-                                  duration=list(emotion_annotations["duration"]),  # duration in seconds
-                                  description=list(emotion_annotations["description"])  # textual descriptors
-                                  )
-raw.set_annotations(raw_annotations)
+        # if the annotation is not a video then just copy it down
+        else:
+            onsets += [(row["onset"] - start_time).total_seconds()]
+            durations += [row["duration"]]
+            descriptions += [row["description"]]
 
-# let's look at the events with the new annotations
-events, event_dict = mne.events_from_annotations(raw)
+    # create a dataframe for the new annotations
+    emotion_annotations = pandas.DataFrame(data=[onsets, durations, descriptions]).transpose()
+    # rename the columns
+    emotion_annotations = emotion_annotations.rename(columns={0: "onset", 1: "duration", 2: "description"})
+
+    # set the new annotations for the raw file
+    raw_annotations = mne.Annotations(onset=list(emotion_annotations["onset"]),  # onset, in seconds
+                                      duration=list(emotion_annotations["duration"]),  # duration in seconds
+                                      description=list(emotion_annotations["description"])  # textual descriptors
+                                      )
+    raw.set_annotations(raw_annotations)
+
+    # let's look at the events with the new annotations
+    events, event_dict = mne.events_from_annotations(raw)
+
+    raws.append(raw)
+"""
+
+# combine all raw files into one large one
+all_raws = mne.concatenate_raws(raws)
+events, event_dict = mne.events_from_annotations(all_raws)
 
 # Pre-Processing
-artifact_removal = raw.copy()
+artifact_removal = all_raws.copy()
 artifact_removal.filter(l_freq=1.0, h_freq=None, n_jobs=-1)  # high-pass filter at 1Hz
-artifact_removal.notch_filter(60.0, n_jobs=-1)  # notch filter at 60Hz
 
 # ICA artifact removal
 ica = mne.preprocessing.ICA(n_components=0.95, random_state=97, max_iter="auto")
@@ -129,7 +147,7 @@ ica.fit(artifact_removal)  # fit the ICA with EEG and EOG information
 
 # Visually inspect the data
 N = ica.n_components_
-ica.plot_properties(raw, picks=list(range(0, N)), psd_args={"fmin": 1.0, "fmax": 60.0})  # further analyze the channels
+ica.plot_properties(all_raws, picks=list(range(0, N)), psd_args={"fmin": 1.0, "fmax": 80.0})  # further analyze the channels
 matplotlib.pyplot.show(block=True)  # wait until all figures are closed
 
 # last chance to un-bad components...
@@ -137,14 +155,14 @@ response = input("Type any bad components (0-{}) that should be marked for exclu
 ica.exclude = [int(x) for x in response.split(" ")]  # mark bad components for removal
 
 # See the changes we've made
-ica.plot_overlay(raw, exclude=ica.exclude, picks='eeg')
+ica.plot_overlay(all_raws, exclude=ica.exclude, picks='eeg')
 matplotlib.pyplot.show(block=True)  # wait until all figures are closed
 
-ica.apply(raw)  # apply ICA to data, removing the artifacts
+ica.apply(all_raws)  # apply ICA to data, removing the artifacts
 
-want_epoch = False
+want_epoch = True
 if want_epoch:
-    epochs = mne.Epochs(raw, events, event_id=event_dict, tmin=-0.2, tmax=30,
+    epochs = mne.Epochs(all_raws, events, event_id=event_dict, tmin=-0.2, tmax=30,
                         preload=False, event_repeated="drop", reject=None, reject_by_annotation=None, proj=False)
 
     relevant = ["OVTK_StimulationId_Label_00", "OVTK_StimulationId_Label_01",
@@ -152,4 +170,4 @@ if want_epoch:
     for i in relevant:
         epochs[i].compute_psd(method="welch", average='mean', picks=['eeg'], fmax=60).plot()
 
-mne.export.export_raw("exp.vhdr", raw, fmt='brainvision', overwrite=True)
+#mne.export.export_raw("exp.vhdr", raw, fmt='brainvision', overwrite=True)
